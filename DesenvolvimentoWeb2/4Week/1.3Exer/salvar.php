b509d71e1ee487fb1c8bc103c40a5a8f3195670c
Conrado <74992540+conradobr1@users.noreply.github.com>
2025-08-12 23:10:50 -0300

    Add files via upload

---
+++ DesenvolvimentoWeb2/4Week/1.3Exer/salvar.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nome'])) {
+    $_SESSION['nome'] = htmlspecialchars(trim($_POST['nome']));
+    header('Location: mostrar.php');
+    exit();
+} else {
+    header('Location: primeira.php');
+    exit();
+}
+
+//Conrado
+?>
+
