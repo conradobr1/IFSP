76cd1659d2456e1efb5fb173d4a90aaea570297c
Conrado <74992540+conradobr1@users.noreply.github.com>
2025-11-03 21:45:06 -0300

    Delete DesenvolvimentoWeb2/11Week/11Week directory

--- DesenvolvimentoWeb2/11Week/11Week/salva_atividade.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'config.php';
-$stmt = $pdo->prepare("INSERT INTO atividades (usuario_id, titulo, comentario) VALUES (?, ?, ?)");
-$stmt->execute([$_SESSION['usuario_id'], $_POST['titulo'], $_POST['comentario']]);
-echo "<script>alert('Atividade criada!');window.location='atividades.php';</script>";
-?>
