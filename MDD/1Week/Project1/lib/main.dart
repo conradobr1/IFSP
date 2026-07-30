edf151c3381fb5b0410ea91eb3f5ac6d0f538642
Conrado <74992540+conradobr1@users.noreply.github.com>
2026-07-29 22:01:43 -0300

    Project 1 Week
    Project  1 Week

---
+++ MDD/1Week/Project1/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      home: Scaffold(
+    body: Center(child: Text('Hello World')),
+    //backgroundColor: Color.fromARGB(122, 0, 255, 221),
+    backgroundColor: Colors.blue,
+  )));
+}
